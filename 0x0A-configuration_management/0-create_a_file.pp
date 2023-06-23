37281b2eac73eed7135cc4f4d77a5193f97b5533
omwami <ianalpha@protonmail.com>
2023-06-24 00:16:18 +0300

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
   ensure  => file,
-  content => 'I love Puppet',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
