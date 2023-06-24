345f2467ffe9545127b5fc1a3ab03f15a8aa7174
omwami <ianalpha@protonmail.com>
2023-06-24 09:33:34 +0300

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# install the flask package
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
