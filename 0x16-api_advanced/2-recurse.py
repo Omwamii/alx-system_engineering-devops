eb515446343a6bc553c1ab709baa0ae77c133ecf
omwami <ianalpha@protonmail.com>
2023-08-10 13:34:37 +0300

    Find all hot posts recursively

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
-""" Return number of subscribers for a sub-reddit
+""" Return list of hot posts for subreddit
 """
 import requests
 
 
-def top_ten(subreddit):
-    """ print top ten posts of subred if valid else None
-    """
-    req_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    sub_data = requests.get(req_url, headers={'User-agent': 'Uniq 0.1'})
-    if sub_data.status_code != 200:
-        return print(None)
-    else:
-        hot = 0
-        for post in sub_data.json()['data']['children']:
-            if hot > 9:
-                break
-            print(post['data']['title'])
-            hot += 1
+def recurse(subreddit, hot_list=[], after=None):
+    req_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'after': after}
+    headers = {'User-agent': 'Uniq 1.0'}
+
+    response = requests.get(req_url, params=params, headers=headers)
+
+    if response.status_code != 200:  # subreddit does not exist
+        return None
+
+    data = response.json().get('data')
+    after = data.get('after')
+
+    def r_pages(subreddit, hot_list, after):
+        """ recursively append hot lists
+        """
+        if after is None:
+            return
+
+        req_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+        params = {'after': after}
+        headers = {'User-agent': 'Uniq 1.0'}
+        response = requests.get(req_url, params=params, headers=headers)
+
+        data = response.json().get('data')
+        children = data.get('children', [])
+        for child in children:
+            hot_list.append(child['data']['title'])
+        after = data.get('after')
+        return r_pages(subreddit, hot_list, after)
+    r_pages(subreddit, hot_list, after)
+    return hot_list
