c8d7d896fea4b6e6c081df8899d545151f159b97
omwami <ianalpha@protonmail.com>
2023-08-10 15:20:13 +0300

    Find all hot posts recursively

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,42 +1,35 @@
 #!/usr/bin/python3
-""" Return list of hot posts for subreddit
+""" module to find all hot posts of subreddit
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ find all hot posts
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    """Fn that that returns list of hot posts for subreddit
+       Point: pagination used to recursively get data
     """
-    req_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    params = {'after': after}
-    head = {'User-agent': 'Ubuntu.20.04:alx.advanced.api:v1.0 (by /u/sk001)'}
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+               "User-Agent": "Ubuntu.20.04:alx.advanced.api:v1.0 (by /u/sk001)"
+    }
 
-    response = requests.get(req_url, params=params, headers=head,
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 50
+    }
+
+    response = requests.get(URL, headers=headers, params=params,
                             allow_redirects=False)
-
-    if response.status_code != 200:  # subreddit does not exist
+    if response.status_code == 404:
         return None
 
-    data = response.json().get('data')
-    after = data.get('after')
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for k in results.get("children"):
+        hot_list.append(k.get("data").get("title"))
 
-    def r_pages(subreddit, hot_list, after):
-        """ recursively append hot lists
-        """
-        if after is None:
-            return
-
-        req_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-        params = {'after': after}
-        head = {'User-agent': 'Ubuntu.20.04:alx.advanced.api:v1.0 \
-                (by /u/sk001)'}
-        response = requests.get(req_url, params=params, headers=head)
-
-        data = response.json().get('data')
-        children = data.get('children', [])
-        for child in children:
-            hot_list.append(child['data']['title'])
-        after = data.get('after')
-        return r_pages(subreddit, hot_list, after)
-    r_pages(subreddit, hot_list, after)
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
